73d60b038ab4f88277ce4391727f2927ac8317af
Daniel Marinescu <dani.marinescu7@gmail.com>
2026-05-09 21:12:21 +0300

    Forced usage of real JWT

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { validateEnv } from './config.js';
 
 dotenv.config();
+validateEnv();
 
 const port = Number(process.env.PORT || 3001);
 
